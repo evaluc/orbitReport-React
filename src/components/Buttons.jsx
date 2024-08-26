fc0765dfad68dfb789e5e423218ab2468a570b0a
evaluc <ealuckow@gmail.com>
2024-08-25 20:56:42 -0500

    Add styling, check formatting + remove stray comments

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,9 @@
+import "./styling.css";
 import satData from "./satData";
 
 const Buttons = ({ filterByType, setSat, orbitTypes }) => {
   return (
-    <div>
+    <div className="flex-container">
       {orbitTypes.map((type, index) => {
         return (
           <button onClick={() => filterByType(type)} key={index}>
