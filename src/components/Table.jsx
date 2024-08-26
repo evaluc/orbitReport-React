79516c283691ad10ac81950da4069130201b1c3b
evaluc <ealuckow@gmail.com>
2024-08-25 20:46:15 -0500

    Complete Table.jsx, adjust variable names in Buttons and App

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,15 +1,26 @@
 const Table = ({ sat }) => {
+
+  const satInfo = sat.map((satellite) => (
+        <tr key={satellite.id}>
+          <td>{satellite.name}</td>
+          <td>{satellite.type}</td>
+          <td>{satellite.launchDate}</td>
+          <td>{satellite.operational ? "Active" : "Inactive"}</td>
+        </tr>
+  ));
+
   return (
     <table>
     <thead>
      <tr>
-       <th>Header TBD</th>
+       <th>Name</th>
+       <th>Type of Satellite</th>
+       <th>Launch Date</th>
+       <th>Status</th>
      </tr>
      </thead>
      <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
+         {satInfo}
      </tbody>
    </table>
   );
